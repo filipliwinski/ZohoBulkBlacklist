addcb2d9c285ae883a61f9f6d2c3dc718695276d
Filip Liwiński <filipliwinski@users.noreply.github.com>
2019-11-07 22:18:37 +0100

    Bulk verify for single tenant.

---
+++ src/ZohoBulkBlacklist/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ZohoBulkBlacklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helo to Zoho Bulk Blacklist tool!");
+            if (args.Length < 2 || args.Length > 3)
+            {
+                Console.WriteLine(@"Provide parameters:
+[0]: path to the file containing the list of currently blocked domains (each domain in separate line)
+[1]: path to the file containing the list from Zoho Mail quarantine (file name: example.com.txt)
+[2]: path to the file containing the list of currently blocked domains from other tenants (optional).");
+                Console.ReadKey();
+            }
+
+            // Add existing SPAM domains
+            var existingDomains = File.ReadAllLines(args[0]);
+            var existingDomainsSet = new HashSet<string>(existingDomains.Length);
+
+            for (int i = 0; i < existingDomains.Length; i++)
+            {
+                existingDomainsSet.Add(existingDomains[i].Trim());
+            }
+
+            File.WriteAllLines(args[0], existingDomainsSet);
+            Console.WriteLine("Existing domains: " + existingDomainsSet.Count);
+
+            var newDomains = new HashSet<string>();
+
+            // Verify domains form other tenants
+            if (args.Length == 3)
+            {
+                var existingDomainsTenants = File.ReadAllLines(args[2]);
+                for (int i = 0; i < existingDomainsTenants.Length; i++)
+                {
+                    var domain = existingDomainsTenants[i].Trim();
+                    if (!existingDomainsSet.Contains(domain) && newDomains.Add(domain))
+                    {
+                        Console.WriteLine($"{newDomains.Count}: {domain}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No other tenants provided.");
+            }
+
+            // Verify new SPAM addresses
+            var addresses = File.ReadAllLines(args[1]);
+
+            for (int i = 0; i < addresses.Length; i++)
+            {
+                if (addresses[i].Contains("@") && 
+                    !addresses[i].Contains("@" + args[1][0..^4]))
+                {
+                    var domain = addresses[i].Trim().Split('@')[1];
+                    if (!existingDomainsSet.Contains(domain) && newDomains.Add(domain))
+                    {
+                        Console.WriteLine($"{newDomains.Count}: {domain}");
+                    }
+                }
+            }
+
+            // Save new SPAM domains
+            var output = new StringBuilder();
+            if (newDomains.Count == 0)
+            {
+                output.Append("No new domains!");
+                Console.WriteLine(output.ToString());
+            }
+            else
+            {
+                var j = 0;
+                var k = 0;
+                output.Append($"[{k}] ");
+                foreach (var item in newDomains)
+                {
+                    if (j++ != 9)
+                    {
+                        output.Append(item).Append(',');
+                    }
+                    else
+                    {
+                        output.Append(item).Append(';').Append($"[{++k}] ");
+                        j = 0;
+                    }
+                }
+            }
+
+            File.WriteAllLines("new" + args[0], output.ToString().TrimEnd(',').TrimEnd(';').Split(';'));
+            Console.WriteLine("All done.");
+        }
+    }
+}
